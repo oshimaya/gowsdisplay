629500b05f07cd3d7e5b09382ae74f5ced142a6d
Yasushi Oshima <oshima-ya@yagoto-urayama.jp>
2018-06-02 08:03:12 +0900

    Add some drawing operation

--- operation.go
+++ operation.go
@@ -27,6 +27,8 @@ package gowsdisplay
 
 import (
 	"errors"
+	"image"
+	"image/color"
 )
 
 func (wsd *WsDisplay) PutPxielArray(px int, py int, p PIXELARRAY) error {
@@ -131,6 +133,148 @@ func (wsd *WsDisplay) Clear() {
 		pix := wsd.GetBufferAsPixel16()
 		for i := range pix {
 			pix[i] = PIXEL16{0, 0}
+		}
+	}
+}
+
+func (wsd *WsDisplay) NewPixel(c color.Color) (p PIXEL) {
+	switch wsd.GetDepth() {
+	case 32:
+		p = new(PIXEL32)
+	case 24:
+		p = new(PIXEL24)
+	case 16:
+		p = new(PIXEL16)
+	default:
+		return nil
+	}
+	p.SetColor(c, wsd.GetRGBmask())
+	return
+}
+
+func (wsd *WsDisplay) SetPixel(px int, py int, p PIXEL) {
+	if px < 0 || py < 0 ||
+		px >= int(wsd.GetWidth()) || py >= int(wsd.GetHeight()) {
+		// Nothing to do. All area is out of screen
+		return
+	}
+
+	depth := int(wsd.GetDepth())
+	switch p.(type) {
+	case *PIXEL32:
+		if depth != 32 {
+			return
+		}
+		pix := wsd.GetBufferAsPixel32()
+		pix[px+py*int(wsd.GetPixelStride())] = *p.(*PIXEL32)
+
+	case *PIXEL24:
+		if depth != 24 {
+			return
+		}
+		pix := wsd.GetBufferAsPixel24()
+		pix[px+py*int(wsd.GetPixelStride())] = *p.(*PIXEL24)
+	case *PIXEL16:
+		if depth != 16 {
+			return
+		}
+		pix := wsd.GetBufferAsPixel16()
+		pix[px+py*int(wsd.GetPixelStride())] = *p.(*PIXEL16)
+	}
+
+}
+
+func (wsd *WsDisplay) DrawBox(rect image.Rectangle, p PIXEL) {
+	for x := rect.Min.X; x < rect.Max.X; x++ {
+		wsd.SetPixel(x, rect.Min.Y, p)
+		wsd.SetPixel(x, rect.Max.Y, p)
+	}
+	for y := rect.Min.Y; y < rect.Max.Y; y++ {
+		wsd.SetPixel(rect.Min.X, y, p)
+		wsd.SetPixel(rect.Max.X, y, p)
+	}
+}
+func (wsd *WsDisplay) FillBox(rect image.Rectangle, p PIXEL) {
+	for y := rect.Min.Y; y < rect.Max.Y; y++ {
+		for x := rect.Min.X; x < rect.Max.X; x++ {
+			wsd.SetPixel(x, y, p)
+		}
+	}
+}
+
+func (wsd *WsDisplay) DrawCircle(px int, py int, r int, p PIXEL) {
+	x := r
+	y := 0
+	d := r*-2 + 3
+	for x >= y {
+		wsd.SetPixel(px+x, py+y, p)
+		wsd.SetPixel(px-x, py+y, p)
+		wsd.SetPixel(px+x, py-y, p)
+		wsd.SetPixel(px-x, py-y, p)
+		wsd.SetPixel(px+y, py+x, p)
+		wsd.SetPixel(px-y, py+x, p)
+		wsd.SetPixel(px+y, py-x, p)
+		wsd.SetPixel(px-y, py-x, p)
+		if d >= 0 {
+			x--
+			d -= x * 4
+		}
+		y++
+		d += y*4 + 2
+	}
+}
+func (wsd *WsDisplay) FillCircle(px int, py int, r int, p PIXEL) {
+	x := r
+	y := 0
+	d := r*-2 + 3
+	for x >= y {
+		for dx := px - x; dx < px+x; dx++ {
+			wsd.SetPixel(dx, py+y, p)
+			wsd.SetPixel(dx, py-y, p)
+		}
+		for dx := px - y; dx < px+y; dx++ {
+			wsd.SetPixel(dx, py+x, p)
+			wsd.SetPixel(dx, py-x, p)
+		}
+		if d >= 0 {
+			x--
+			d -= x * 4
+		}
+		y++
+		d += y*4 + 2
+	}
+}
+func (wsd *WsDisplay) DrawLine(p0 image.Point, p1 image.Point, p PIXEL) {
+	sx := -1
+	sy := -1
+	dx := p0.X - p1.X
+	if dx < 0 {
+		dx = -dx
+		sx = 1
+	}
+	dy := p0.Y - p1.Y
+	if dy < 0 {
+		dy = -dy
+		sy = 1
+	}
+
+	px := p0.X
+	py := p0.Y
+
+	er := dx - dy
+	for {
+		wsd.SetPixel(px, py, p)
+		if px == p1.X && py == p1.Y {
+			break
+		}
+		e2 := er * 2
+		if e2 > -dy {
+			er -= dy
+			px += sx
+		}
+		if e2 < dx {
+			er += dx
+			py += sy
 		}
 	}
 }
